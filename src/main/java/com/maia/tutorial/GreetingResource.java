0f09d468c5457fdd59be2c0fc01d65639699aa9d
Tiago Maia <tiagomvf@gmail.com>
2024-07-25 17:14:04 -0300

    Fix test

--- src/main/java/com/maia/tutorial/GreetingResource.java
+++ src/main/java/com/maia/tutorial/GreetingResource.java
@@ -3,10 +3,8 @@ package com.maia.tutorial;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 
 @Path("/hello")
 public class GreetingResource {
@@ -16,9 +14,9 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    @Path("/{id}")
-    public Response hello(@PathParam("id") String id) {
-        sender.add("Hello from Quarkus REST" + id);
-        return Response.ok().build();
+    public String hello() {
+        String messagem = "Hello from Quarkus REST";
+        sender.add(messagem);
+        return messagem;
     }
 }
